a1542aa7d044a055465759ba1f5e03b76fb4e58a
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-06 16:33:22 -0300

    changes

--- src/features/options/ColorOfPlayers.jsx
+++ src/features/options/ColorOfPlayers.jsx
@@ -3,11 +3,9 @@ import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/lib/css/styles.css";
 import { useDispatch } from "react-redux";
 import { setColorOfPlayers } from "../../redux/teamsSlice";
-import { useState } from "react";
 
 function ColorOfPLayers() {
   const dispatch = useDispatch();
-  const [team, setTeam] = useState("teamA");
   const [color, setColor] = useColor("hex", "#121212");
 
   const handleSetColor = (color) => {
