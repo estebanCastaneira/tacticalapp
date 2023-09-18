037d18c937bb1dc35e32700dc11882a4321899b3
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-18 17:55:39 -0300

    normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
+import "./normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
